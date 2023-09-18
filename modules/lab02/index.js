2cb3c587b0a801d487ad1ec1f225218296174808
marco <102998873+marcolap13@users.noreply.github.com>
2023-09-18 21:22:22 +0200

    Update index.js
    3.4. Sending back HTML (hard level)

--- modules/lab02/index.js
+++ modules/lab02/index.js
@@ -1,10 +1,23 @@
 const http = require('http')
 
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+
 const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World\n')
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write(content)
+  res.end()
 }
 
 http
-  .createServer(serverHandle)
-  .listen(8080)
+.createServer(serverHandle)
+.listen(8080)
