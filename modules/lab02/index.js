451a86cf2e870c7788c92eae2fadbfc7c008c893
marco <102998873+marcolap13@users.noreply.github.com>
2023-09-18 21:20:27 +0200

    modif index.js
    3.3. Define callback function (hard level)

---
+++ modules/lab02/index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const serverHandle = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World\n')
+}
+
+http
+  .createServer(serverHandle)
+  .listen(8080)
