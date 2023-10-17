51fbb410fc4dcb9f665bf5c7a6d3daa07bb1b67a
marcolap13 <mla.marco13@gmail.com>
2023-10-17 18:35:02 +0200

    feat: reorganize files

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/header";
+
+export default function Index() {
+  return (
+    <div className="text-center mt-20 font-sans">
+      <h1 className="text-2xl mb-10 text-gray-800">Welcome</h1>
+      <p className="text-base text-gray-700 mb-10">
+        This is our web-site for web-tech
+      </p>
+    </div>
+  );
+}
