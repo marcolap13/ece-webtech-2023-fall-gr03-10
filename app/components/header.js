6a3460a1c7594afb836b695912c12c242d184dbd
marcolap13 <mla.marco13@gmail.com>
2023-10-17 21:01:18 +0200

    feat: add about and contact

--- app/components/header.js
+++ app/components/header.js
@@ -7,7 +7,7 @@ export default function Index() {
     <div className="bg-yellow p-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <div className="text-4xl font-bold">ALIBOBO</div>
+          <div className="text-4xl font-bold text-orange-700">ALIBOBO</div>
           <div className="relative">
             <input
               type="text"
