c61ffb97305f853132891048b16bdfd9fbf7f219
marcolap13 <mla.marco13@gmail.com>
2023-11-15 15:52:52 +0100

    feat: add supabase options

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { UserProvider } from '../components/UserContext';
-import '../styles/globals.css';
+import "../styles/global.css";
+import RootLayout from "../components/Layout";
+import { UserProvider } from "../components/UserContext";
 
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 function MyApp({ Component, pageProps }) {
+
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
   return (
     <UserProvider>
+      <RootLayout>
+      <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
+    </SessionContextProvider>
+      </RootLayout>
     </UserProvider>
   );
 }
-export default MyApp;
+
+export default MyApp; 
---
+++ supabase/README.md
@@ -0,0 +1,27 @@
+
+# Supabase Docker
+
+This is a minimal Docker Compose setup for self-hosting Supabase. Follow the steps [here](https://supabase.com/docs/guides/hosting/docker) to get started.
+
+
+# Get the code
+git clone --depth 1 https://github.com/supabase/supabase
+
+# Go to the docker folder
+cd supabase/docker
+
+# Copy the fake env vars
+cp .env.example .env
+
+# Pull the latest images
+docker compose pull
+
+# Start the services (in detached mode)
+docker compose up -d
+
+
+#check
+docker compose ps
+
+#stop
+docker compose stop
