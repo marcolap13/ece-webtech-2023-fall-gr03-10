117cb1d3a8c76996c2d64a2e8883eb035f9f8196
Rayan ZOUCH <101805291+RZOUCH@users.noreply.github.com>
2023-12-22 23:35:43 +0100

    feat: articles and login

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,16 @@
 import "../styles/global.css";
 import RootLayout from "../components/Layout";
 import { UserProvider } from "../components/UserContext";
+import { supabase } from '../utils/supabaseClients'; // Importez l'instance de Supabase
 
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 function MyApp({ Component, pageProps }) {
-
-  const [supabaseClient] = useState(() => createPagesBrowserClient())
   return (
     <UserProvider>
       <RootLayout>
-      <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} />
-    </SessionContextProvider>
+        <Component {...pageProps} />
       </RootLayout>
     </UserProvider>
   );
 }
 
-export default MyApp; 
+export default MyApp;
--- pages/api/articles/[slug].js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const db = require('../articles.js');
-  const article = db.find( article => article.slug === req.query.slug)
-  if( !article ) return res.status(404).json('Wrong article')
-  res.status(200).json(article)
-}
