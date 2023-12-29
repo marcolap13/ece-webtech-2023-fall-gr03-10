f43f45a902e8424309d9e238af5e001c35f99dca
marcolap13 <mla.marco13@gmail.com>
2023-12-29 18:00:45 +0100

    fix: fix layout problem for build

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,15 @@ import React from "react";
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useTheme, theme } from "../context/ThemeContext";
+import { useTheme } from "../context/ThemeContext";
 
 const Layout = ({ children, title, description }) => {
+  const { theme } = useTheme();
+
   const pageTitle =
     typeof title === "string" ? `Webtech - ${title}` : "Webtech";
 
-  const Layoutstyle = {
+  const layoutStyle = {
     backgroundColor: theme === 'dark' ? 'var(--background-color-dark)' : 'var(--background-color-light)',
     color: theme === 'dark' ? 'var(--text-color-dark)' : 'var(--text-color-light)',
     padding: "20px",
@@ -27,7 +29,7 @@ const Layout = ({ children, title, description }) => {
         style={{
           backgroundImage: `url(https://i.ibb.co/GQTqG8K/background.jpg)`,
           backgroundSize: "cover",
-          Layoutstyle
+          ...layoutStyle
         }}
       >
         <Header />
